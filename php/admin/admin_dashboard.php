b13124baef6cf7177d2c1a377f2c1e1b397b3c56
dipesh <dipeshmahatto@gmail.com>
2024-01-23 00:16:15 +0545

    admin

--- php/admin/admin_dashboard.php
+++ php/admin/admin_dashboard.php
@@ -5,13 +5,31 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../../css/admin_dashboard.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
     <title>Document</title>
 </head>
 
 <body>
     <div class="container">
-        <div class="nav">echo</div>
-        <div class="content">hello</div>
+        <div class="nav">
+            <div class="profile">
+                <img src="../../img/admin_profile.jpg" alt="">
+                <br>
+                <h2>Admin</h2>
+                <input type="submit" value="Change password">
+            </div>
+            <div class="operation">
+                <a href="#"><i class="fa-solid fa-gauge-high"></i>  Dashboard</a>
+                <a href="#"><i class="fa-solid fa-user-doctor"></i>Doctor</a>
+                <a href="#"><i class="fa-solid fa-bed-pulse"></i>Patient</a>
+                <a href="#"><i class="fa-solid fa-person-circle-check"></i> Approvel</a>
+            </div>
+        </div>
+        <div class="content">
+            <div class="top"></div>
+            <div class="middle"></div>
+            <div class="table"></div>
+        </div>
     </div>
 </body>
 
