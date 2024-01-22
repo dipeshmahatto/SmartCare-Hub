c277d095c3a083fa9b64e601961643716a5d2947
dipesh <dipeshmahatto@gmail.com>
2024-01-22 23:07:37 +0545

    admin dashboard started

---
+++ php/admin/admin_dashboard.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../css/admin_dashboard.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="nav">echo</div>
+        <div class="content">hello</div>
+    </div>
+</body>
+
+</html>
