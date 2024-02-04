64e83d94195d57de60e958f790482ddeb25554a6
dipesh <dipeshmahatto@gmail.com>
2024-02-04 22:35:44 +0545

    top css modified

--- php/patient/appointment.php
+++ php/patient/appointment.php
@@ -13,6 +13,7 @@ include "../database.php";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../../css/patient_dashboard.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <title>Patient Dashboard</title>
 </head>
