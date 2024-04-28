6837bd3a18461b8753cbdc3bc4135cc629fc0764
dipesh <dipeshmahatto@gmail.com>
2024-04-28 21:55:06 +0545

    some changes

---
+++ php/sampleeee.php
@@ -0,0 +1,45 @@
+<?php
+include "database.php";
+$sql = "SELECT * FROM doctor";
+$result = mysqli_query($conn, $sql);
+?>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style.css">
+    <title>SmartCare Hub</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="doctors">
+            <?php while ($row = $result->fetch_assoc()): ?>
+                <div class="list" id="doc">
+                    <img src="../img/admin_profile.jpg" alt="Image Not Found">
+
+                    <h3>
+                        <?php echo "Name :" . " " . $row['fullName']; ?>
+                    </h3>
+                    <p>
+                        <?php echo "Speciality :" . " " . $row['speciality']; ?>
+                    <p>
+                    <p>
+                        <?php echo "Qualification :" . " " . $row['qualification']; ?>
+                    </p>
+                    <p>
+                        <?php echo "Email :" . " " . $row['email']; ?>
+                    </p>
+                </div>
+            <?php endwhile; ?>
+        </div>
+    </div>
+
+</body>
+
+</html>
+
+<?php
+include("footer.php");
+?>
